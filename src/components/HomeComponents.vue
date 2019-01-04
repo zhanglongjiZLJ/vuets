be5fa9e2538df80e6d338b14b25d6cb11ce4c0fa
ZhangLongji <zhanglongji@juanet.com>
2019-01-04 12:31:21 +0800

    修改home组件为HomeComponents

---
+++ src/components/HomeComponents.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>home组件
+        <div>{{age}}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Prop, Vue } from "vue-property-decorator";
+export default class HomeComponents extends Vue {
+    //接收父组件的参数
+    @Prop(Number) age!: number;
+    mounted(){
+        console.log(this.age)
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
